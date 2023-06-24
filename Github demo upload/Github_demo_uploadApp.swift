2271b0f649db44d4d37f577a7029e6210b257453
Sidrah Javed <sidrahjaved14@gmail.com>
2023-06-24 13:52:11 +0400

    Initial Commit

---
+++ Github demo upload/Github_demo_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_demo_uploadApp.swift
+//  Github demo upload
+//
+//  Created by Sidrah Javed on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_demo_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
